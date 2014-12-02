3f552edd711241a24bf5d6eb65ba4c22a10367e5
Eric Bullock <bullocke@bu.edu>
2014-12-02 14:00:57 -0500

    testing

--- js/scripts.js
+++ js/scripts.js
@@ -28,7 +28,6 @@ google.maps.visualRefresh = true;
 
 var map;
 function initialize() {
-  var mapOptions = {
  
  map = new google.maps.Map(document.getElementById('map-canvas'), {
     zoom: 2,
@@ -42,4 +41,4 @@ function initialize() {
 google.maps.event.addDomListener(window, 'load', initialize);
 
 
-});
+}
