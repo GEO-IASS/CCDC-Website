d10d413313fbab2201c4ea037a6060b31e6290b9
Eric Bullock <bullocke@bu.edu>
2014-12-02 14:05:58 -0500

    testing

--- js/scripts.js
+++ js/scripts.js
@@ -26,19 +26,64 @@ $('#nav .navbar-nav li>a').click(function(){
 // enable the visual refresh
 google.maps.visualRefresh = true;
 
+
+
 var map;
 function initialize() {
  
  map = new google.maps.Map(document.getElementById('map-canvas'), {
+n initialize() {
+  var mapOptions = {
+    zoom: 2,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+        // try HTML5 geolocation
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+					position.coords.longitude);
+
+      var infowindow = new google.maps.InfoWindow({
+        map: map,
+        position: pos,
+        content: 'Location found using HTML5.'
+      });
+
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // browser doesn't support geolocation
+    handleNoGeolocation(false);
+  }
+}
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(60, 105),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
+}
+
     zoom: 2,
     center: {lat: 42.3, lng: -71.06}
     });
-
   map.data.loadGeoJson('https://raw.githubusercontent.com/bullocke/Landsat-Database/master/PRmap.geojson');
 
 }
-
-google.maps.event.addDomListener(window, 'load', initialize);
+oogle.maps.event.addDomListener(window, 'load', initialize);
 
 
 }
